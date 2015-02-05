bb69a5073d21aee5e9725b1860216ebbf6e7acae
habnpam <habnpam@gmail.com>
2015-02-04 20:26:47 -0800

    initial commit

---
+++ desktop/src/com/blellow/pop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blellow.pop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blellow.pop.BlellowPop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlellowPop(), config);
+	}
+}
